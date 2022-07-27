3dbbdfbce91047bd6adc1f70520f6a89212118d6
Level-Up-4 <vikash-verma-profile>
2022-07-27 10:38:12 +0530

    added

---
+++ sumoftwonumbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+   int a,b;
+   cout<<"Enter the value of a an b";
+   cin>>a>>b;
+   int sum=a+b;
+   cout<<"Sum of two number are "<<sum;
+}
