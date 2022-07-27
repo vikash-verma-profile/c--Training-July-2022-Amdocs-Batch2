7d8249d225157a74ec6d6231a70d0f3372aecc2c
Level-Up-4 <vikash-verma-profile>
2022-07-27 11:45:43 +0530

    added

---
+++ arrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int a[10];
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<"enter the value of a["<<i<<"]= ";
+        cin>>a[i];
+    }
+    for (int j = 0; j < 5; j++)
+    {
+         cout<<" a["<<j<<"]= "<<a[j]<<endl;
+    }
+    
+}
